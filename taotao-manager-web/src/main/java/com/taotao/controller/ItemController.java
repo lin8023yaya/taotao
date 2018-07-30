cc0da15ab51549a24abaff0f86ae1dab8911ca2b
lin <1159647024@qq.com>
2018-07-31 00:34:35 +0800

    commit

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,30 @@
+package com.taotao.controller;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 商品管理controller
+ * @ClassName: ItemController
+ * @author: Mr.Li
+ * @create: 2018-07-31 00:27
+ **/
+@Controller
+@RequestMapping("/")
+@ResponseBody
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    public TbItem getItemById(@PathVariable Long itemId){
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+    };
+}
